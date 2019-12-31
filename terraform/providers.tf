130ebaec63ecbdf1f32de8d486e7dbe6b78496c0
Nick Law <nick@nicholaslaw.com.au>
2020-01-01 09:42:10 +1100

    Run fmt

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,7 +1,7 @@
 provider "google" {
-  version     = "~> 3.3"
-  project     = "chatstatz-control"
-  region      = local.region
+  version = "~> 3.3"
+  project = "chatstatz-control"
+  region  = local.region
   # Add the path to your credentials.json file here be for running this templare
   # credentials = file("/path/to/credentials.json")
 }
